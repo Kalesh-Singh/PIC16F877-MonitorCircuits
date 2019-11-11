0b6a667e07c7e2e47ccc69be2fbe7876ff12f911
Kalesh-Singh <kaleshsingh96@gmail.com>
2019-11-11 13:30:00 -0500

    Added include, removes unecessary dedlarations

--- Singh_MonitorCircuits.asm
+++ Singh_MonitorCircuits.asm
@@ -15,8 +15,7 @@
 ;   - The output of the PIR is connected to pin RB1.
 ;   - The input of the buzzer is connected to pin RD2.
 ;**************************************************************************
-
-	list P=16F877
+#include <p16f877.inc>
 
 ;--------------------------------------------------------------------------
 ; Program Code
@@ -32,23 +31,15 @@
 ; Main Program
 ;--------------------------------------------------------------------------
 
-STATUS	equ	0x03	; Declare STATUS SFR - Bank 0
-PORTB	equ	0x06    ; Declare PORTB SFR  - Bank 0
-TRISB	equ	0x86    ; Declare TRISB SFR  - Bank 1
-PORTD	equ	0x08    ; Declare PORTD SFR  - Bank 0
-TRISD	equ	0x88    ; Declare TRISD SFR  - Bank 1
-
-P0	equ	0x05	; Constant P0 = STATUS<5> (Bank Select)
-P1	equ	0x06	; Constant P1 = STATUS<6> (Bank Select)
 PIR	equ	0x01	; Constant PIR = PORTB<1> (Motion Detection Input)
 BUZ	equ	0x02	; Constant BUZ = PORTD<2> (Relay switch)
 
 	org     0x0005
 
-start	bsf	STATUS, P0	; Select Bank 1
+start	bsf	STATUS, RP0	; Select Bank 1
 	bsf	TRISB, PIR	; Set TRISB<1> = 1 so PORTB<1> is an INPUT
 	bcf	TRISD, BUZ	; Set TRISD<2> = 0 so PORTD<2> is an OUTPUT
-	bcf	STATUS, P0	; Select Bank 0
+	bcf	STATUS, RP0	; Select Bank 0
 
 ; Monitor PIR - Motion is detected if PORTB<1> = 0
 again	bcf	PORTD, BUZ	; Turn the buzzer off
